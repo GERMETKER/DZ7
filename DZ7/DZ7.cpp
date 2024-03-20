b129280220eb042c237e2d99fe7ee23437d75f74
GERMETKER <160734087+GERMETKER@users.noreply.github.com>
2024-03-20 18:21:59 +0500

    Update DZ7.cpp

--- DZ7/DZ7.cpp
+++ DZ7/DZ7.cpp
@@ -1,9 +1,8 @@
-﻿#include <Windows.h>
-#include <iostream>
+﻿#include <iostream>
 #include <random>
+#include <Windows.h>
 void Ex1();
 void Ex2();
-void Ex3();
 int main()
 {
     SetConsoleCP(1251);
@@ -19,106 +18,49 @@ int main()
     {
         Ex2();
     }
-    else if (choose == 3)
-    {
-        Ex3();
-    }
     return 0;
 }
 void Ex1()
 {
     const int size = 10;
     int arr[size];
-    int max = 0, min = 0;
     for (int i = 0; i < size; i++)
     {
-        arr[i] = 1 + rand() % 100;
         if (i == 0)
         {
-            max = arr[i];
-            min = arr[i];
+            std::cout << "Введите первый элемент массива\n";
+            std::cin >> arr[i];
         }
-        if (arr[i] > max)
+        else
         {
-            max = arr[i];
-        }
-        else if (arr[i] < min)
-        {
-            min = arr[i];
+            arr[i] = arr[i - 1] * 2;
         }
     }
+    std::cout << "------------------------------------------\n";
     for (int i = 0; i < size; i++)
     {
         std::cout << arr[i] << "\n";
     }
-    std::cout << "\n-----------------------------------------\n";
-    std::cout << max << "\n";
-    std::cout << min << "\n";
-
 }
 void Ex2()
 {
-    int start = 0, end = 0, num = 0, sum = 0;
     const int size = 10;
     int arr[size];
     for (int i = 0; i < size; i++)
     {
-        arr[i] = 1 + rand() % 100;
-    }
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << arr[i] << "\n";
-    }
-    std::cout << "Введите начало диапазона\n";
-    std::cin >> start;
-    std::cout << "Введите начало диапазона\n";
-    std::cin >> end;
-    std::cout << "Введите число\n";
-    std::cin >> num;
-    for (int i = start - 1; i < end; i++)
-    {
-        int n = arr[i];
-        if (n < num)
-        {
-            sum = sum + arr[i];
-        }
-    }
-    std::cout << "Сумма: " << sum << "\n";
-}
-void Ex3()
-{
-    int start = 0, end = 0, max = 0, min = 0;
-    const int size = 12;
-    int arr[size];
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << "Введите прибыль за " << i + 1 << " месяц\n";
-        std::cin >> arr[i];
-    }
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << arr[i] << "\n";
-    }
-    std::cout << "Введите начало диапазона\n";
-    std::cin >> start;
-    std::cout << "Введите начало диапазона\n";
-    std::cin >> end;
-    for (int i = start - 1; i < end; i++)
-    {
         if (i == 0)
         {
-            max = arr[i];
-            min = arr[i];
+            std::cout << "Введите первый элемент массива\n";
+            std::cin >> arr[i];
         }
-        if (arr[i] > max)
+        else
         {
-            max = arr[i];
-        }
-        else if (arr[i] < min)
-        {
-            min = arr[i];
+            arr[i] = arr[i - 1] + 1;
         }
     }
-    std::cout << "Максимальный доход: " << max << "\n";
-    std::cout << "Минимальный доход: " << min << "\n";
+    std::cout << "------------------------------------------\n";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << "\n";
+    }
 }
